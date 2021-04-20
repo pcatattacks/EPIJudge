f065f1847fa3f178906bb25f10bd2d65affde46c
Pranav Dhingra <pranav.dhingra@gmail.com>
2021-04-20 16:41:02 -0500

    5.17: is_valid_soduku.py

--- epi_judge_python/is_valid_sudoku.py
+++ epi_judge_python/is_valid_sudoku.py
@@ -4,6 +4,44 @@ from test_framework import generic_test
 # Check if a partially filled matrix has any conflicts.
 def is_valid_sudoku(partial_assignment):
     # TODO - you fill in here.
+
+    def valid(row):
+        holder = [0] * 10 # for easier indexing
+        for i in row:
+            if i == 0:
+                continue
+            if i == holder[i]:
+                return False
+            else:
+                holder[i] = i
+        return True
+
+    def valid_block(block_x, block_y):
+        holder = [0] * 10
+        for x in range(block_x, block_x + 3):
+            for y in range(block_y, block_y + 3):
+                num = partial_assignment[x][y]
+                if num == 0:
+                    continue
+                if num == holder[num]:
+                    return False
+                else:
+                    holder[num] = num
+        return True
+
+    n = len(partial_assignment)
+    
+    # check rows and cols
+    for x in range(n):
+        if not valid(partial_assignment[x]) or not valid([partial_assignment[y][x] for y in range(n)]):
+            return False
+    
+    # check blocks
+    for x in range(0, n, 3):
+        for y in range(0, n, 3):
+            if not valid_block(x, y):
+                return False
+
     return True
 
 
